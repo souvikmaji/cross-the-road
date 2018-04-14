a35a2e36bbf3e0ee740932666e6037a9a72a6258
souvikmaji <souvikmaji94@gmail.com>
2018-04-15 00:52:39 +0530

    using gulp task runner

--- README.md
+++ README.md
@@ -3,17 +3,6 @@
 
 Doro wants to go to the pool. To do so, she must cross a road full of dangerous monsters. ONLY YOU CAN SAVE HER.
 
-To guide Doro to the pool, open `index.html` in your browser and the use arrow keys to navigate.
+To guide Doro to the pool, run `gulp`, which will create a `dist` subdirectory, open `dist/index.html` in your browser and the use arrow keys to navigate.
 
 ---
-#### Create a new release
-
-Set the GITHUB token environment variable
-```bash
-export GITHUB_TOKEN=[github-secret-token]
-```
-And use
-```bash
-gulp release
-```
-to bump the version, commit the changes and push to github account.
--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,37 @@
 var gulp = require("gulp");
-var runSequence = require("run-sequence");
-var conventionalGithubReleaser = require("conventional-github-releaser");
-var bump = require("gulp-bump");
-var gutil = require("gulp-util");
-var git = require("gulp-git");
-var fs = require("fs");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var imagemin = require("gulp-imagemin");
+var cache = require("gulp-cache");
 
-gulp.task("github-release", function(done) {
-  conventionalGithubReleaser(
-    {
-      type: "oauth",
-      token: process.env.GITHUB_TOKEN
-    },
-    {
-      preset: "angular"
-    },
-    done
-  );
+gulp.task("scripts", function() {
+  gulp.
+    src(["js/resources.js", "js/engine.js", "js/app.js"]).
+    pipe(concat("main.js")).
+    pipe(uglify()).
+    pipe(gulp.dest("dist/js/"));
 });
 
-gulp.task("bump-version", function() {
-  // We hardcode the version change type to 'patch' but it may be a good idea to
-  // use minimist (https://www.npmjs.com/package/minimist) to determine with a
-  // command argument whether you are doing a 'major', 'minor' or a 'patch' change.
-  return gulp
-    .src(["./bower.json", "./package.json"])
-    .pipe(bump({ type: "patch" }).on("error", gutil.log))
-    .pipe(gulp.dest("./"));
+gulp.task("images", function() {
+  return gulp.
+    src("images/*").
+    pipe(cache(imagemin({
+          optimizationLevel: 5,
+          progressive: true,
+          interlaced: true
+        }))).
+    pipe(gulp.dest("dist/images/"));
 });
 
-gulp.task("commit-changes", function() {
-  return gulp
-    .src(".")
-    .pipe(git.add())
-    .pipe(git.commit("[Prerelease] Bumped version number"));
+gulp.task("copy", function() {
+  gulp.src("index.html").pipe(gulp.dest("dist/"));
+  gulp.src("css/*").pipe(gulp.dest("dist/css/"));
 });
 
-gulp.task("push-changes", function(cb) {
-  git.push("origin", "master", cb);
+gulp.task("watch", function() {
+  gulp.watch("js/**.js", ["scripts"]);
+  gulp.watch(["css/**.css", "index.html"], ["copy"]);
+  gulp.watch("images/**", ["images"]);
 });
 
-gulp.task("create-new-tag", function(cb) {
-  var version = getPackageJsonVersion();
-  git.tag(version, `Created Tag for version: ${version}`, function(error) {
-    if (error) {
-      return cb(error);
-    }
-    git.push("origin", "master", { args: "--tags" }, cb);
-  });
-
-  function getPackageJsonVersion() {
-    // We parse the json file instead of using require because require caches
-    // multiple calls so the version number won't be updated
-    return JSON.parse(fs.readFileSync("./package.json", "utf8")).version;
-  }
-});
-
-gulp.task("release", function(callback) {
-  runSequence(
-    "bump-version",
-    "commit-changes",
-    "push-changes",
-    "create-new-tag",
-    "github-release",
-    function(error) {
-      if (error) {
-        console.log(error.message);
-      } else {
-        console.log("RELEASE FINISHED SUCCESSFULLY");
-      }
-      callback(error);
-    }
-  );
-});
-
-gulp.task("default", function() {
-  // place code for your default task here
-});
+gulp.task("default", ["scripts", "images", "copy", "watch"]);
